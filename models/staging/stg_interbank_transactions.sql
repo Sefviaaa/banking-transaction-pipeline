aca8e8087d3ea1b683d062abbcdc76e26fdddcae
Sefvia Lie <sefviaasdfghjkl@gmail.com>
2026-01-09 03:57:51 +0000

    add staging, marts for banking transaction pipeline

---
+++ models/staging/stg_interbank_transactions.sql
@@ -0,0 +1,13 @@
+SELECT
+  TIMESTAMP(timestamp) AS transaction_ts,
+  from_bank,
+  from_account,
+  to_bank,
+  to_account,
+  SAFE_CAST(amount_received AS FLOAT64) AS amount_received,
+  receiving_currency,
+  SAFE_CAST(amount_paid AS FLOAT64) AS amount_paid,
+  payment_currency,
+  TRIM(UPPER(payment_format)) AS payment_format
+FROM {{ source('interbank_raw', 'transactions') }}
+WHERE timestamp IS NOT NULL
